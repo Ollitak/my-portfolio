8bbbbad88b21acfde3a15dc7c64e8d77a983986a
Ollitak <olli.takaanttila@gmail.com>
2022-02-08 14:47:27 +0200

    Implement introduction component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react"
+import Introduction from "./components/introduction/introduction"
 
 function App() {
   return (
     <div className="App">
-      asdasd
+      <Introduction />
     </div>
   )
 }
---
+++ src/components/introduction/Introduction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Introduction = () => {
+  return (
+    <div>
+
+    </div>
+
+  )
+
+
+}
+
+export default Introduction
